c898960092fb62689f505a7ea1b084a5cea135ec
Kian <kian.p.m.007@gmail.com>
2021-08-09 14:22:12 -0500

    visual updates to the dock

--- src/components/Dock.vue
+++ src/components/Dock.vue
@@ -21,12 +21,15 @@ export default {
 .dock {
   position: fixed;
   width: 90%;
-  height: 70px;
+  height: 75px;
   background-image: linear-gradient(to left, #e2e2e2dc, #cac8ccdc, #e2e2e2dc);
   backdrop-filter: saturate(210%) blur(60px);
   border-radius: 20px;
   bottom: 5px;
 
+  max-width: 1611px;
+
+  border: 1px solid black;
   box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.2), 0 8px 40px rgba(0, 0, 0, 0.55);
 }
 </style>
