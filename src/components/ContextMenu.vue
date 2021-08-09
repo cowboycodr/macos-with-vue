4c7ce0a06d0ec4ce2a60f9ee8fe0db81ae884092
Kian <kian.p.m.007@gmail.com>
2021-08-09 13:58:32 -0500

    context menu positioning logic

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -4,7 +4,11 @@
       <li>Finder</li>
     </ul>
     <ul>
-      <li><a href="https://github.com/cowboycodr/macos-with-vue" target="_blank">GitHub</a></li>
+      <li>
+        <a href="https://github.com/cowboycodr/macos-with-vue" target="_blank"
+          >GitHub</a
+        >
+      </li>
     </ul>
   </div>
 </template>
@@ -28,9 +32,23 @@ export default {
       else {
         var menu = document.getElementById("contextMenu");
 
-        menu.style.display = "block";
-        menu.style.left = e.pageX + "px";
-        menu.style.top = e.pageY + "px";
+        menu.style.display = "block"
+
+        if (window.innerWidth - e.pageX > menu.offsetWidth) {
+          menu.style.left = e.pageX + 10 + "px";
+        } else {
+          menu.style.left = e.pageX - menu.clientWidth - 10 + "px"; 
+        }
+
+        if (window.innerHeight - e.pageY > menu.offsetHeight) {
+          console.log(window.innerHeight, e.pageY, menu.offsetHeight)
+
+          menu.style.top = e.pageY + 10 + "px";
+        } else {
+          console.log(menu.clientHeight)
+
+          menu.style.top = e.pageY - menu.clientHeight + "px"
+        }
       }
     }
   },
@@ -44,12 +62,13 @@ export default {
   background: rgba(216, 216, 216, 0.5);
   backdrop-filter: blur(60px) saturate(210%);
 
-  border: .5px solid rgba(0, 0, 0, .2);
-  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, .2),0 8px 40px rgba(0, 0, 0, .55);
+  border: 0.5px solid rgba(0, 0, 0, 0.2);
+  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.2),
+    0 8px 40px rgba(0, 0, 0, 0.55);
   border-radius: 9px;
   overflow: auto;
   text-align: start;
-  min-width: 100px;
+  min-width: 200px;
   z-index: 9999;
 }
 
@@ -58,7 +77,7 @@ export default {
   margin: 0;
   padding: 3px;
 
-  border: .25px solid rgba(255, 255, 255, .2);
+  border: 0.25px solid rgba(255, 255, 255, 0.2);
 }
 
 .context-menu ul li {
@@ -70,11 +89,7 @@ export default {
 
 .context-menu ul li a {
   text-decoration: none;
-  color: black;
-}
-
-.context-menu ul li a:hover {
-  color: white;
+  color: inherit;
 }
 
 .context-menu ul li:hover {
